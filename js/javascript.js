5b449d86684f825d48957168f7b6aa3fa11f53c2
Antonio Freitas <133685290+AF-Github1@users.noreply.github.com>
2026-03-08 20:12:14 -0100

    Adicionado lógica formulario

--- js/javascript.js
+++ js/javascript.js
@@ -180,3 +180,26 @@ function focusOut(element) {
       { x: 0, opacity: 1, display: 'block', duration: 0.4, ease: 'power2.out' }
     )
 }
+
+const validateEmail = (email) => {
+  const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/
+  return emailRegex.test(email)
+}
+
+function hasNumbers (t) {
+  var regex = /\d/g
+  return regex.test(t)
+}
+
+function checkForm() {
+  const name = document.getElementById('name').value
+  const email = document.getElementById('email').value
+  const subject = document.getElementById('subject').value
+  const message = document.getElementById('message').value
+
+  if (validateEmail (email) && !hasNumbers(name) && subject !== '' && message !== '') {
+    alert(name + ', o seu formulário foi enviado com sucesso')
+  } else {
+    alert('Formulário não enviado, confirme as informações submetidas! \nTodos os campos são obrigatórios')
+  }
+}
