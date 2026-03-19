9eb5c6b1199dc94c5a741ee28dd401989e5924c5
julia <2024114388@uac.pt>
2026-03-19 19:47:54 -0100

    Update form.js

--- js/form.js
+++ js/form.js
@@ -15,9 +15,15 @@ function checkForm () {
   const email = document.getElementById('email').value
   const subject = document.getElementById('subject').value
   const message = document.getElementById('message').value
+  const phoneNumber = document.getElementById('phone-number').value
+  const phoneEmpty = phoneNumber === ''
+  // "/^\d{6,15}$/" Verifica se o resultado tem apenas dígitos entre 6 a 15 caracteres
+  const phoneValid = phoneEmpty || /^\d{6,15}$/.test(phoneNumber.replace(/[\s\-().]/g, ''))
 
-  if (validateEmail(email) && !hasNumbers(name) && subject !== '' && message !== '') {
+  if (validateEmail(email) && !hasNumbers(name) && subject !== '' && message !== '' && (phoneEmpty || phoneValid)) {
     alert(name + ', o seu formulário foi enviado com sucesso')
+  } else if (!phoneValid) {
+    alert('Número de telefone inválido. Insira apenas dígitos (6 a 15 números).')
   } else {
     alert('Formulário não enviado, confirme as informações submetidas! \nTodos os campos são obrigatórios')
   }
