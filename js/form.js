1112690464068ef98f0380a421e3fbbd27441b81
Antonio Freitas <133685290+AF-Github1@users.noreply.github.com>
2026-03-25 16:49:50 -0100

    Export e import adicionado

--- js/form.js
+++ js/form.js
@@ -1,90 +1,8 @@
 
 
-class Formulario {
-  /*
-   Esta classe representa o formulário de contacto e contém métodos para a validação dos dados inseridos no formulário
+import { Formulario } from './formClass.js'
 
-   * @param {string} name - Nome do utilizador
-   * @param {string} email - Endereço de email a validar
-   * @param {string} selectSubject - Assunto selecionado das opções pré definidas
-   * @param {string} customSubject - Assunto escrito manualmente por utilizador
-   * @param {string} message - Texto da mensagem
-   * @param {string} phoneNumber - Número de telefone (opcional)
-   */
-  constructor(name, email, selectSubject, customSubject, message, phoneNumber) {
-    this.name = name
-    this.email = email
-    this.selectSubject = selectSubject
-    this.customSubject = customSubject
-    this.message = message
-    this.phoneNumber = phoneNumber
-  }
-
-  get isPhoneEmpty() {
-    /*
-    Verifica se o campo de telefone está vazio.
-    */
-    return this.phoneNumber === ''
-  }
-
-  get finalSubject() {
-    /*
-    Obtem o assunto final, sendo esta a opção pré definida ou texto inserido por um utilizador
-    */
-    return this.selectSubject === 'outro' ? this.customSubject : this.selectSubject
-  }
-
-
-
-  validateEmail() {
-  /*
-  
-  Função que verifica se uma determinada string está no formato de xxx@yyy.zzz, em que x poderá assumir qualquer value, y poderá assumir 
-  valuees de letras e números, e z poderá assumir apenas valuees de letras, devendo a sequência de z ter 2 ou mais caracteres.
-  xxx e yyy deverão ser separados com o simbolo de arroba { @ } | yyy e zzz deverão ser separados por um ponto { . }
-  Caso a string do email não esteja num formato válido, a função devolve False
-
-  */
-  const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9-]+\.[a-z]{2,}$/
-  return emailRegex.test(this.email)
-  }
-
-  hasNumbers() {
-  /*
-  Verifica através de regex se uma determinada string contém caracteres que não sejam números inteiros, devolvendo False se a string não os conter
-  */
-  var regex = /\d/g
-  return regex.test(this.name)
-  }
-
-  isPhoneValid() {
-    /*
-    Utiliza-se regex para verificar se o telefone contém apenas dígitos e entre 6 a 15 caracteres
-    Sendo o numero de telefone opcional, a falta complete de um número de telefone é considerada válida  
-    */
-    if (this.phoneNumber !== '') {
-    const cleanNumber = this.phoneNumber.replace(/[\s\-().]/g, '')
-    return /^\d{6,15}$/.test(cleanNumber)
-    }
-
-    return true
-  }
-
-  isValid() {
-    /*
-    Verifica se todas as condições para o formulário ser válido estão preenchidas (email válido, nome sem numeros, mensagem ou assunto não vazios, numero de telefone válido)    
-    */
-    return (
-      this.validateEmail() &&
-     !this.hasNumbers() &&
-      this.finalSubject !== '' && 
-      this.message !== '' &&
-      this.isPhoneValid()
-    )
-  }
-}
-
-function fillMessage () {
+export function fillMessage () {
  
  /*
  Preenche a mensagem e o assunto com texto pré-definido consoante a opção selecionada.
@@ -124,7 +42,7 @@ function fillMessage () {
 
 
 
-function checkForm () {
+export function checkForm () {
   /*
 
   Esta função informa o utilizador sobre o sucesso ou a falha na submissão do formulário, usando a classe Formulario e os métodos associados para validar a validade da informação
@@ -143,3 +61,17 @@ function checkForm () {
     alert('Formulário não enviado, confirme as informações submetidas! \nTodos os campos são obrigatórios')
   }
 }
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+const subjectSelect = document.getElementById('subject')
+const formElement = document.getElementById('contacto-form')
+
+subjectSelect.addEventListener('change', fillMessage)
+formElement.addEventListener('submit', (event) => {
+    checkForm()
+  })
+
+})
